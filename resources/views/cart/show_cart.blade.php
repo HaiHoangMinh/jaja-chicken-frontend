74b714fd5885987c13529d49ea8da876f1139ee1
HaiHoangMinh <haibg1998b@gmail.com>
2021-09-01 15:14:33 +0700

    final_project

--- resources/views/cart/show_cart.blade.php
+++ resources/views/cart/show_cart.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('title')
-    <title>Giỏ hàng</title>
+<title>Giỏ hàng</title>
 @endsection
 
 
@@ -9,94 +9,100 @@
     <div class="container">
         <div class="breadcrumbs">
             <ol class="breadcrumb">
-              <li><a href="#">Home</a></li>
-              <li class="active">Shopping Cart</li>
+                <li><a href="#">Home</a></li>
+                <li class="active">Shopping Cart</li>
             </ol>
         </div>
-       
+
         <div class="table-responsive cart_info">
             <form>
                 @csrf
-            <table class="table table-condensed">
-                <thead>
-                    <tr class="cart_menu">
-                        <td class="image">Hình ảnh</td>
-                        <td class="description">Tên sản phẩm</td>
-                        <td class="price">Giá</td>
-                        <td></td>
-                        <td></td>
-                        <td class="quantity">Số lượng</td>
-                        <td class="total">Thành tiền</td>
-                        <td></td>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    @if(Session::get('cart')==true)
-                            @php
-								$total = 0;
-                                $shipping = 0;
-						    @endphp
+                <table class="table table-condensed">
+                    <thead>
+                        <tr class="cart_menu">
+                            <td class="image">Hình ảnh</td>
+                            <td class="description">Tên sản phẩm</td>
+                            <td class="price">Giá</td>
+                            <td></td>
+                            <td></td>
+                            <td class="quantity">Số lượng</td>
+                            <td class="total">Thành tiền</td>
+                            <td></td>
 
-                    @foreach(Session::get('cart') as $key=>$cart)
-                            @php
-								$subtotal = $cart['product_price']*$cart['product_qty'];
-								$total+=$subtotal;
-							@endphp
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if(Session::get('cart')==true)
+                        @php
+                        $total = 0;
+                        $shipping = 0;
+                        @endphp
 
-                    <tr>
-                        <td class="cart_product">
-                            <img src="{{config('app.base_url').$cart['product_image']}}" alt="" width="150" height="150">
-                        </td>
-                        <td class="cart_description">
-                            <h4><a href="">{{$cart['product_name']}}</a></h4>
-                            <p>Product ID: {{$cart['product_id']}} </p>
-                        </td>
-                        <td class="cart_price">
-                            <p>{{number_format($cart['product_price'],0,',','.')}}đ</p>
-                        </td>
-                        <td></td>
-                        <td></td>
-                        <td class="cart_quantity">
-                            <div class="cart_quantity_button">
-                                <button type="button" class="btn btn-default reduction" 
-                                data-id="{{$cart['product_id']}}"
-                                data-session_id="{{$cart['session_id']}}"> - </button>
-                                
-                                <input class="cart_quantity_input value_{{$cart['product_id']}}" type="number"
-                                 value="{{$cart['product_qty']}}" min="1" id="value" disabled>
-                                 <button type="button" class="btn btn-default increase" 
-                                 data-id="{{$cart['product_id']}}" 
-                                 data-session_id="{{$cart['session_id']}}"> + </button>  
-                                
-                            </div>
-                        </td>
-                        <td class="cart_total">
-                            <p class="cart_total_price" id="sub_total_{{$cart['product_id']}}">{{number_format($subtotal,0,',','.')}}đ</p>
-                        </td>
-                        <td class="cart_delete">
-                            <a class="cart_quantity_delete" href="{{url('/delete-product/'.$cart['session_id'])}}"><i class="fa fa-times"></i></a>
-                        </td>
-                    </tr>
-                    
-                    @endforeach
-                    <tr>
-                        {{-- <td><input type="submit" value="Cập nhật giỏ hàng" name="update_qty" class="btn btn-default check_out"></td> --}}
-                        <td><a class="btn btn-default check_out" href="{{url('/delete-all')}}">Xóa toàn bộ sản phẩm </a></td>
-                       
-                    </tr>
-                   
-                </tbody>
-                
+                        @foreach(Session::get('cart') as $key=>$cart)
+                        @php
+                        $subtotal = $cart['product_price']*$cart['product_qty'];
+                        $total+=$subtotal;
+                        @endphp
+
+                        <tr>
+                            <td class="cart_product">
+                                <img src="{{config('app.base_url').$cart['product_image']}}" alt="" width="150"
+                                    height="150">
+                            </td>
+                            <td class="cart_description">
+                                <h4><a href="">{{$cart['product_name']}}</a></h4>
+                                <p>Product ID: {{$cart['product_id']}} </p>
+                            </td>
+                            <td class="cart_price">
+                                <p>{{number_format($cart['product_price'],0,',','.')}}đ</p>
+                            </td>
+                            <td></td>
+                            <td></td>
+                            <td class="cart_quantity">
+                                <div class="cart_quantity_button">
+                                    <button type="button" class="btn btn-default reduction"
+                                        data-id="{{$cart['product_id']}}" data-session_id="{{$cart['session_id']}}"> -
+                                    </button>
+
+                                    <input class="cart_quantity_input value_{{$cart['product_id']}}" type="number"
+                                        value="{{$cart['product_qty']}}" min="1" id="value" disabled>
+                                    <button type="button" class="btn btn-default increase"
+                                        data-id="{{$cart['product_id']}}" data-session_id="{{$cart['session_id']}}"> +
+                                    </button>
+
+                                </div>
+                            </td>
+                            <td class="cart_total">
+                                <p class="cart_total_price" id="sub_total_{{$cart['product_id']}}">
+                                    {{number_format($subtotal,0,',','.')}}đ</p>
+                            </td>
+                            <td class="cart_delete">
+                                <a class="cart_quantity_delete"
+                                    href="{{url('/delete-product/'.$cart['session_id'])}}"><i
+                                        class="fa fa-times"></i></a>
+                            </td>
+                        </tr>
+
+                        @endforeach
+                        <tr>
+                            {{-- <td><input type="submit" value="Cập nhật giỏ hàng" name="update_qty" class="btn btn-default check_out"></td> --}}
+                            <td><a class="btn btn-default check_out" href="{{url('/delete-all')}}">Xóa toàn bộ sản phẩm
+                                </a></td>
+
+                        </tr>
+
+                    </tbody>
+
             </form>
             <tr>
                 <td>
                     <br>
                     <form action="{{url('/check-coupon')}}" method="POST">
-                        @csrf             
-                        <input type="text" class="form-control" placeholder="Nhập mã giảm giá" name="coupon" style="margin-left: 18px">
+                        @csrf
+                        <input type="text" class="form-control" placeholder="Nhập mã giảm giá" name="coupon"
+                            style="margin-left: 18px" required>
                         <input type="submit" value="Tính mã giảm giá" class="btn btn-default check_out"
-                        name="check_coupon">
+                            name="check_coupon">
                     </form>
                     <?php
                     
@@ -128,18 +134,19 @@
                         $message = Session::put('message',null);
                     ?>
                     @if(Session::get('coupon'))
-                    <a class="btn btn-default check_out" href="{{url('/unset-coupon')}}" >Xóa mã </a>
+                    <a class="btn btn-default check_out" href="{{url('/unset-coupon')}}">Xóa mã </a>
                     @endif
                 </td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
-                <td >
-                    <li style="margin-top: -15px">Tổng tiền món ăn: <span id="total">{{number_format($total,0,',','.')}}đ</span></li>
+                <td>
+                    <li style="margin-top: -15px">Tổng tiền món ăn: <span
+                            id="total">{{number_format($total,0,',','.')}}đ</span></li>
                     <li>Tiền giảm: {{number_format($result,0,',','.')}}đ</span></li>
                     <li>Tiền phải trả: <span id="total2">{{number_format($total-$result,0,',','.')}}đ</span></li>
-                    
+
                 </td>
                 <br>
                 <td>
@@ -163,25 +170,23 @@
                 </td>
             </tr>
             @else
-                <center><h3>
+            <center>
+                <h3>
                     <strong>
                         @php
-                        echo "Vui lòng thêm sản phẩm vào giỏ hàng !" 
+                        echo "Vui lòng thêm sản phẩm vào giỏ hàng !"
                         @endphp
                     </strong>
-                </h3    >
-                   
-                </center>
-                
+                </h3>
+
+            </center>
+
             @endif
             </table>
-            
+
         </div>
     </div>
-</section> <!--/#cart_items-->
+</section>
+<!--/#cart_items-->
 
 @endsection
-
-
-
-
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,118 +1,133 @@
-<header id="header"><!--header-->
-		<div class="header_top"><!--header_top-->
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="contactinfo">
-							<ul class="nav nav-pills">
-								<li><a href="https://www.facebook.com/JaJaChickenVietnam" target="_blank"><i class="fa fa-phone"></i> +84 0383766181</a></li>
-								<li><a href="https://www.facebook.com/JaJaChickenVietnam" target="_blank"><i class="fa fa-envelope"></i> hoangloi.sumida@gmail.com</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<div class="social-icons pull-right">
-							<ul class="nav navbar-nav">
-								<li><a href="https://www.facebook.com/JaJaChickenVietnam" target="_blank"><i class="fa fa-facebook"></i></a></li>
-								<li><a href="https://www.instagram.com/jaja_friedchicken/" target="_blank"><i class="fa fa-instagram" target="_blank"></i></a></li>
-								<li><a href="https://mail.google.com/mail/u/0/#sent" target="_blank"><i class="fa fa-google-plus" target="_blank"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/header_top-->
-		
-		<div class="header-middle"><!--header-middle-->
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4 clearfix">
-						<div class="logo pull-left">
-							<a href="{{route('home')}}">
-								<img src="/images/logo-removebg-preview.png" alt="" height=120px width=150px/>
-							</a>
-						</div>
-		
-					</div>
-					<div class="col-md-8 clearfix">
-						<div class="shop-menu clearfix pull-right">
-							<ul class="nav navbar-nav">
-								
-								<?php 
+<header id="header">
+    <!--header-->
+    <div class="header_top">
+        <!--header_top-->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="contactinfo">
+                        <ul class="nav nav-pills">
+                            <li><a href="https://www.facebook.com/JaJaChickenVietnam" target="_blank"><i
+                                        class="fa fa-phone"></i> +84 0383766181</a></li>
+                            <li><a href="https://www.facebook.com/JaJaChickenVietnam" target="_blank"><i
+                                        class="fa fa-envelope"></i> hoangloi.sumida@gmail.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="social-icons pull-right">
+                        <ul class="nav navbar-nav">
+                            <li><a href="https://www.facebook.com/JaJaChickenVietnam" target="_blank"><i
+                                        class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://www.instagram.com/jaja_friedchicken/" target="_blank"><i
+                                        class="fa fa-instagram" target="_blank"></i></a></li>
+                            <li><a href="https://mail.google.com/mail/u/0/#sent" target="_blank"><i
+                                        class="fa fa-google-plus" target="_blank"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/header_top-->
+
+    <div class="header-middle">
+        <!--header-middle-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 clearfix">
+                    <div class="logo pull-left">
+                        <a href="{{route('home')}}">
+                            <img src="/images/logo-removebg-preview.png" alt="" height=100px width=100px />
+                        </a>
+                    </div>
+
+                </div>
+                <div class="col-md-8 clearfix">
+                    <div class="shop-menu clearfix pull-right">
+                        <ul class="nav navbar-nav">
+
+                            <?php 
 									$customer_id = Session::get('customer_id');
 									$customer_name = Session::get('customer_name');
 									if ($customer_id != null) {
 										
 									
 								?>
-								<li><span style="color: white; font-size: 14px">Xin chào: {{$customer_name}}</span></li>
-								<br/>
-								<li><a href="{{URL::to('/khach-hang')}}"><i class="fa fa-user"></i>Tài khoản</a></li>
-								<li><a href="{{URL::to('/payment')}}"><i class="fa fa-credit-card"></i> Thanh toán</a></li>
-								<?php 
+                            <li><span style="color: white; font-size: 14px">Xin chào: {{$customer_name}}</span></li>
+                            <br />
+                            <li><a href="{{URL::to('/khach-hang')}}"><i class="fa fa-user"></i>Tài khoản</a></li>
+                            <!-- <li><a href="{{URL::to('/payment')}}"><i class="fa fa-credit-card"></i> Thanh toán</a></li> -->
+                            <?php 
 									} else{
 
 									
 								?>
-								<li><a href="{{URL::to('/checkout')}}"><i class="fa fa-credit-card"></i> Thanh toán</a></li>
-								<?php 
+                            <!-- <li><a href="{{URL::to('/checkout')}}"><i class="fa fa-credit-card"></i> Thanh toán</a></li> -->
+                            <?php 
 									}
 								?>
-								
-								<li><a href="{{URL::to('/gio-hang')}}"><i class="fa fa-shopping-cart"></i>
-									 Giỏ hàng
-									 <span id="show-cart"></span>
-									</a></li>
-								<?php 
+
+                            <li><a href="{{URL::to('/gio-hang')}}"><i class="fa fa-shopping-cart"></i>
+                                    Giỏ hàng
+                                    <span id="show-cart"></span>
+                                </a></li>
+                            <?php 
 									$customer_id = Session::get('customer_id');
 									if ($customer_id!=null) {
 
 								?>
-								<li><a href="{{URL::to('/logout-checkout')}}"><i class="fa fa-sign-out"></i> Đăng xuất</a></li>
-								<?php 
+                            <li><a href="{{URL::to('/logout-checkout')}}"><i class="fa fa-sign-out"></i> Đăng xuất</a>
+                            </li>
+                            <?php 
 									} else{
 
 									
 								?>
-								<li><a href="{{URL::to('/login-checkout')}}"><i class="fa fa-lock"></i> Đăng nhập</a></li>
-								<?php 
+                            <li><a href="{{URL::to('/login-checkout')}}"><i class="fa fa-lock"></i> Đăng nhập</a></li>
+                            <?php 
 									}
 							
 								?>
-								
-								
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/header-middle-->
-	
-		<div class="header-bottom"><!--header-bottom-->
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-9">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-						</div>
-						@include('components.main_menu')
-					</div>
-					<div class="col-sm-3">
-						<form action="{{URL::to('/tim-kiem')}}" method="POST">
-							@csrf
-							<div class="search_box pull-right">
-								<input type="text" name="keyword" placeholder="Tìm kiếm" />
 
-							</div>
-						</form>
-						
-					</div>
-				</div>
-			</div>
-		</div><!--/header-bottom-->
-	</header><!--/header-->
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/header-middle-->
+
+    <div class="header-bottom">
+        <!--header-bottom-->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    @include('components.main_menu')
+                </div>
+                <div class="col-sm-3">
+                    <form action="{{URL::to('/tim-kiem')}}" method="POST">
+                        @csrf
+                        <div class="search_box pull-right">
+                            <input type="text" name="keyword" placeholder="Tìm kiếm" />
+
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/header-bottom-->
+</header>
+<!--/header-->
